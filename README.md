4996ab087ea426b3f8dfc2e3d9a7f1ec04b3ef1b
Matt Strimas-Mackey <mstrimas@gmail.com>
2019-01-29 13:57:17 -0500

    run matrix defined; test run completed

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # Marxan vs Integer Linear Progamming Paper
 
 Comparison of simulated annealing (with Marxan) versus integer linear programming (ILP; with the R package `prioritizr`) for solving conservation prioritizaiton problems. For the ILP prioritizations, we use both Gurobi and Symphony, to provide examples of closed- and open-source ILP solvers, respectively.
-
-The main analsyis occurs in the following scripts:
-
-- `01_time-vs-iterations.r`: increasing the number of simulated annealing iterations when running Marxan leads to lower cost solutions. In this script, we vary the number of iterations and see how it affects both cost (relative to the true optimum found with Gurobi) and execution time.
-- `02_n-features.r`: in general, problems with more features are more challening to solve, leading to longer execution times. This script explores how cost and execution time are influenced by the number of features, for the different solvers.
--- nplcc_runs.r
+++ nplcc_runs.r
@@ -2,46 +2,214 @@ library(raster)
 library(prioritizr)
 library(marxan)
 library(foreach)
-library(doParallel)
+library(here)
 library(tidyverse)
 select <- dplyr::select
+walk(list.files("R", full.names = TRUE), source)
+prioritizr_timed <- add_timer(prioritizr::solve)
 
 # load nplcc data ----
 
 # species list
-download.file("https://cornell.box.com/s/zi83x5wbyi1tfrjhzc9oiy59dvw76y7h")
+species <- here("data", "nplcc_species.csv") %>% 
+  read_csv() %>% 
+  mutate(id = as.integer(id))
 
-
-
-species <- read_csv("data/nplcc_species.csv")
-sp_lookup <- deframe(species %>% select(id, species_code))
-
-# cost
-r_cost <- raster("data/nplcc_cost.tif") %>% 
-  setNames("cost")
-n_cell <- cellStats(!is.na(r_cost), sum)
-
-# species occupancy
-r_occ <- list.files("data/species/", full.names = TRUE) %>% 
-  stack()
-names(r_occ) <- names(r_occ) %>% 
-  str_extract("(?<=nplcc_occ_)[a-z]+") %>%
-  {sp_lookup[.]}
-cost_occ <- stack(r_cost, r_occ) %>% 
-  rasterToPoints()
+# cost and occupancy
+nplcc_file <- here("data", "nplcc_cost_occupancy.rds")
+if (!file.exists(nplcc_file)) {
+  "https://s3.amazonaws.com/marxan-vs-ilp/nplcc_cost_occupancy.rds" %>% 
+    download.file(destfile = nplcc_file)
+}
+cost_occ <- readRDS(nplcc_file) %>% 
+  mutate(pu = row_number()) %>% 
+  select(pu, everything())
+# split out cost and occupancy
+cost <- select(cost_occ, id = pu, cost) %>% 
+  arrange(id)
+occ <- select(cost_occ, -cost) %>% 
+  gather("species_code", "amount", -pu) %>% 
+  inner_join(species %>% select(species_code, species = id), 
+             by = "species_code") %>% 
+  select(pu, species, amount)
+rm(cost_occ)
 
 
 # setup runs ----
 
 # define run matrix
+# runs <- expand.grid(
+#   target = seq(0.1, 0.9, by = 0.1),
+#   n_features = round(seq(10, 72, length.out = 5)),
+#   n_pu = round(nrow(cost) / 4^(4:0)),
+#   marxan_iterations = c(1e5, 1e6, 1e7, 1e8, 1e9)) %>% 
+#   # nest on marxan iterations since this doesn't affect ilp runs
+#   nest(marxan_iterations) %>% 
+#   rename(marxan = data) %>% 
+#   mutate(run_id = row_number()) %>% 
+#   select(run_id, everything())
+
+# for testing
 runs <- expand.grid(
-  target = c(0.5, seq(0.1, 0.9, by = 0.1), 0.95),
-  n_features = c(30, 40, 50, 60, 72),
-  n_pu = round(n_cell / 10^(0:4)),
-  marxan_iterations = c(1e5, 1e6, 1e7, 1e8, 1e9)
-)
+  target = c(0.25, 0.75),
+  n_features = c(30, 72),
+  n_pu = round(nrow(cost) / 10^(4:3)),
+  marxan_iterations = c(1e5, 1e6)) %>% 
+  # nest on marxan iterations since this doesn't affect ilp runs
+  nest(marxan_iterations) %>% 
+  rename(marxan = data) %>% 
+  mutate(run_id = row_number()) %>% 
+  select(run_id, everything())
 
 # fixed run parameters
-ilp_solvers <- c("marxan", "gurobi", "rsymphony")
 ilp_gap <- 0.1
-marxan_replicates <- 100
+marxan_reps <- 100
+marxan_spf <- 10
+marxan_path <- here("marxan", "MarOpt_v243_Mac64")
+
+
+# iterate over runs ----
+
+# clean up old files
+gurobi_dir <- here("output", "gurobi")
+list.files(gurobi_dir, full.names = TRUE) %>% unlink()
+rsymphony_dir <- here("output", "rsymphony")
+list.files(rsymphony_dir, full.names = TRUE) %>% unlink()
+marxan_dir <- here("output", "marxan")
+list.files(marxan_dir, full.names = TRUE) %>% unlink()
+
+set.seed(1)
+runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows) %do% {
+  r <- runs[run, ]
+  str_glue_data(r, "Run ", run, 
+                ": Target {target}; Features {n_features}; PUs {n_pu}") %>% 
+    message()
+  
+  # sample species and planning units
+  features <- species %>% 
+    select(id, name = species_code) %>% 
+    sample_n(size = r$n_features) %>% 
+    arrange(id) %>% 
+    as.data.frame(stringsAsFactors = FALSE)
+  cost_ss <- cost %>% 
+    sample_n(size = r$n_pu, replace = FALSE) %>% 
+    arrange(id)
+  rij <- filter(occ, species %in% features$id, pu %in% cost_ss$id) %>% 
+    arrange(pu, species)
+  targets <- group_by(rij, species) %>% 
+    summarize(amount = sum(amount, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    transmute(id = species, amount = r$target * amount)
+  features <- inner_join(features, targets, by = "id")
+  
+  # ilp 
+  p <- problem(cost_ss, features = features, rij = rij, cost_column = "cost") %>% 
+    add_min_set_objective() %>%
+    add_relative_targets(r$target) %>%
+    add_binary_decisions()
+  # gurobi
+  s_gur <- p %>% 
+    add_gurobi_solver(gap = ilp_gap) %>% 
+    prioritizr_timed()
+  # save
+  "gurobi_target-{target}_features-{n_features}_pu-{n_pu}.rds" %>% 
+    str_glue_data(r, .) %>% 
+    file.path(gurobi_dir, .) %>% 
+    saveRDS(s_gur, .)
+  cost_gurobi <- attr(s_gur$result, "objective")
+  r$gurobi <- list(tibble(n_solutions = 1,
+                          cost = cost_gurobi, 
+                          time = s_gur$time[["elapsed"]]))
+  rm(s_gur)
+  
+  # symphony
+  s_sym <- p %>% 
+    add_rsymphony_solver(gap = ilp_gap * cost_gurobi) %>% 
+    prioritizr_timed()
+  # save
+  "rsymphony_target-{target}_features-{n_features}_pu-{n_pu}.rds" %>% 
+    str_glue_data(r, .) %>% 
+    file.path(rsymphony_dir, .) %>% 
+    saveRDS(s_sym, .)
+  cost_gurobi <- attr(s_sym$result, "objective")
+  r$rsymphony <- list(tibble(n_solutions = 1,
+                             cost = cost_gurobi, 
+                             time = s_sym$time[["elapsed"]]))
+  rm(s_sym)
+  
+  # marxan
+  # data
+  m_data <- MarxanData(pu = mutate(cost_ss, status = 0L), 
+                       species = transmute(features, id = id, 
+                                           target = amount, 
+                                           spf = marxan_spf), 
+                       puvspecies = rij, 
+                       boundary = NULL)
+  # loop over marxan iterations
+  r_marxan <- foreach(i_marxan = seq_len(nrow(r$marxan[[1]])), 
+                      .combine = bind_rows) %do% {
+    r_marxan <- r$marxan[[1]][i_marxan, , drop = FALSE]
+    message(paste("  Marxan:", r_marxan$marxan_iterations, "iterations"))
+    # options
+    m_opts <- MarxanOpts(BLM = 0, NCORES = 1L)
+    m_opts@NUMREPS <- as.integer(marxan_reps)
+    m_opts@NUMITNS <- as.integer(r_marxan$marxan_iterations)
+    m_opts@NUMTEMP <- as.integer(ceiling(m_opts@NUMITNS * 0.2))  
+    m_unsolved <- MarxanUnsolved(opts = m_opts, data = m_data)
+    # solve
+    td <- file.path(tempdir(), "marxan-run")
+    dir.create(td, recursive = TRUE, showWarnings = FALSE)
+    write.MarxanData(m_unsolved@data, td)
+    # need to write twice to avoid segfault, no idea why???
+    write.MarxanOpts(m_unsolved@opts, td, seed = 1)
+    write.MarxanOpts(m_unsolved@opts, td, seed = 1)
+    file.copy(marxan_path, td)
+    system(paste0("chmod +x ", file.path(td, basename(marxan_path))))
+    cmd <- paste0("cd ", td, "; ./", basename(marxan_path), " input.dat -s")
+    m_time <- system.time(safely(system)(cmd))
+    # save
+    f_marxan <- file.path(td, "output_sum.csv")
+    if (file.exists(f_marxan)) {
+      "marxan_target-{target}_features-{n_features}_pu-{n_pu}_iters-" %>% 
+        str_glue_data(cbind(r, r_marxan), .) %>%
+        paste0(i_marxan, ".csv") %>% 
+        file.path(marxan_dir, .) %>% 
+        file.copy(f_marxan, .)
+      s_marxan <- read.csv(file.path(td, "output_sum.csv"), 
+                           stringsAsFactors = FALSE)
+      r_marxan$n_solutions <- sum(s_marxan$Shortfall == 0)
+      if (r_marxan$n_solutions > 0) {
+        r_marxan$cost <- filter(s_marxan, Shortfall == 0) %>% 
+          pull(Cost) %>% 
+          min()
+      } else {
+        r_marxan$cost <- NA_real_
+      }
+      r_marxan$time <- m_time["elapsed"]
+    } else {
+      r_marxan$n_solutions <- NA_integer_
+      r_marxan$cost <- NA_real_
+      r_marxan$time <- NA_real_
+    }
+    unlink(td, recursive = TRUE)
+    r_marxan
+  }
+  r$marxan <- list(r_marxan)
+  r
+}
+
+# unnest
+runs_g <- runs %>% 
+  mutate(solver = "gurobi") %>% 
+  select(run_id, solver, target, n_features, n_pu, gurobi) %>% 
+  unnest()
+runs_s <- runs %>% 
+  mutate(solver = "rsymphony") %>% 
+  select(run_id, solver, target, n_features, n_pu, rsymphony) %>% 
+  unnest()
+runs_m <- runs %>% 
+  mutate(solver = "marxan") %>% 
+  select(run_id, solver, target, n_features, n_pu, marxan) %>% 
+  unnest()
+runs_long <- bind_rows(runs_g, runs_s, runs_m)
+write_csv(runs_long, here("output", "ilp-comparison-runs.csv"))
