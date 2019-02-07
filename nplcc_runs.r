32cba29c51fc26156a7cbf7becc79f6f9b43ae92
ricschuster <mail@richard-schuster.com>
2019-02-07 07:56:41 -0800

    Marxan cores

--- nplcc_runs.r
+++ nplcc_runs.r
@@ -204,7 +204,7 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows) %do% {
     # data
     m_data@species$spf <- r_marxan$spf
     # options
-    m_opts <- MarxanOpts(BLM = 0, NCORES = 1L, VERBOSITY = 3L)
+    m_opts <- MarxanOpts(BLM = 0, NCORES = 12L, VERBOSITY = 3L)
     m_opts@NUMREPS <- as.integer(marxan_reps)
     m_opts@NUMITNS <- as.integer(r_marxan$marxan_iterations)
     m_opts@NUMTEMP <- as.integer(ceiling(m_opts@NUMITNS * 0.2))
