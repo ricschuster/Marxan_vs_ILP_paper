d83a2f84c28b1b199ccf0ef89621052e14a26ced
Richard Schuster <ricschuster@users.noreply.github.com>
2019-02-24 14:11:51 -0800

    post processing update

--- nplcc_post_processing.R
+++ nplcc_post_processing.R
@@ -106,7 +106,10 @@ rl_filt <- runs_long %>% filter(n_features == 72 & n_pu == 148510 &
 
 rl_filt <- rl_filt %>%
   mutate(deltaC = (cost - filter(rl_filt, solver == 'gurobi')$cost)/filter(rl_filt, solver == 'gurobi')$cost * 100,
-         deltaT = cost - filter(rl_filt, solver == 'gurobi')$cost)
+         deltaT = cost - filter(rl_filt, solver == 'gurobi')$cost,
+         deltaTM = (time - filter(rl_filt, solver == 'gurobi')$time)/filter(rl_filt, solver == 'gurobi')$time * 100,
+         deltaTT = time - filter(rl_filt, solver == 'gurobi')$time
+         )
 
 
 (p1 <- ggplot(data=rl_filt, aes(x = target, y = time, group = solver)) +
@@ -117,8 +120,19 @@ rl_filt <- rl_filt %>%
     scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target)    
 )
 
+(p2 <- ggplot(data=rl_filt, aes(x = target, y = deltaTM, group = solver)) +
+    ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations = 1E+08 \n mean time + mean cost for Marxan") +
+    #ylab("Mean processing time [sec]") +
+    geom_line(aes(color=solver))+
+    geom_point(aes(color=solver)) +
+    geom_text(aes(label = ifelse(solver == "gurobi", "",as.character(paste0(round(deltaTM/100,2),""))), hjust = 0.5, vjust = -0.7)) +
+    
+    scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target) +
+    scale_y_continuous("Differnce to fastest solver [multiplier of best time]", labels = as.character(c(0, 200, 400, 600)), breaks = c(0, 20000, 40000, 60000))
+)
 
-(p2 <- ggplot(data=rl_filt, aes(x = target, y = cost, group = solver)) +
+
+(p3 <- ggplot(data=rl_filt, aes(x = target, y = cost, group = solver)) +
     ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations = 1E+08 \n mean time + mean cost for Marxan") +
     ylab("Solution cost [$]") +
     geom_line(aes(color=solver))+
@@ -126,7 +140,7 @@ rl_filt <- rl_filt %>%
     scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target)
 )
 
-(p3 <- ggplot(data=rl_filt, aes(x = target, y = deltaC, group = solver)) +
+(p4 <- ggplot(data=rl_filt, aes(x = target, y = deltaC, group = solver)) +
     ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations = 1E+08 \n mean time + mean cost for Marxan") +
     ylab("Delta cost [%] with optimal cost as baseline") +
     geom_line(aes(color=solver))+
@@ -148,24 +162,39 @@ rl_filt <- runs_long %>% filter(n_features == 72 & n_pu == 148510 &
 
 rl_filt <- rl_filt %>%
   mutate(deltaC = (cost - filter(rl_filt, solv_it == 'gurobi')$cost)/filter(rl_filt, solv_it == 'gurobi')$cost * 100,
-         deltaT = cost - filter(rl_filt, solv_it == 'gurobi')$cost)
+         deltaT = cost - filter(rl_filt, solv_it == 'gurobi')$cost,
+         deltaTM = (time - filter(rl_filt, solv_it == 'gurobi')$time)/filter(rl_filt, solv_it == 'gurobi')$time * 100,
+         deltaTT = time - filter(rl_filt, solv_it == 'gurobi')$time
+  )
 
-(p4 <- ggplot(data=rl_filt, aes(x = target, y = time, group = solv_it)) +
+
+(p5 <- ggplot(data=rl_filt, aes(x = target, y = time, group = solv_it)) +
     ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations > 10k \n mean time + mean cost for Marxan") +    ylab("Mean processing time [sec]") +
     geom_line(aes(color=solv_it))+
     geom_point(aes(color=solv_it)) +
     scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target)    
 )
 
+(p6 <- ggplot(data=rl_filt, aes(x = target, y = deltaTM, group = solv_it)) +
+    ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations = 1E+08 \n mean time + mean cost for Marxan") +
+    #ylab("Mean processing time [sec]") +
+    geom_line(aes(color=solv_it))+
+    geom_point(aes(color=solv_it)) +
+    geom_text(aes(label = ifelse(solv_it == "gurobi", "",as.character(paste0(round(deltaTM/100,2),""))), hjust = 0.5, vjust = -0.7)) +
+    
+    scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target) +
+    scale_y_continuous("Differnce to fastest solver [multiplier of best time]", labels = as.character(c(0, 200, 400, 600)), breaks = c(0, 20000, 40000, 60000))
+)
 
-(p5 <- ggplot(data=rl_filt, aes(x = target, y = cost, group = solv_it)) +
+
+(p7 <- ggplot(data=rl_filt, aes(x = target, y = cost, group = solv_it)) +
     ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations > 10k \n mean time + mean cost for Marxan") +    ylab("Solution cost [$]") +
     geom_line(aes(color=solv_it))+
     geom_point(aes(color=solv_it))+
     scale_x_continuous("Target [%]", labels = as.character(rl_filt$target * 100), breaks = rl_filt$target)
 )
 
-(p6 <- ggplot(data=rl_filt, aes(x = target, y = deltaC, group = solv_it)) +
+(p8 <- ggplot(data=rl_filt, aes(x = target, y = deltaC, group = solv_it)) +
     ggtitle("Marxan - ILP: # features = 72; # pu's = 148510; # iterations > 10k \n mean time + mean cost for Marxan") +
     ylab("Delta cost [%] with optimal cost as baseline") +
     geom_line(aes(color=solv_it))+
@@ -181,4 +210,18 @@ ggsave(here("figures","p3.png"), p3)
 ggsave(here("figures","p4.png"), p4)
 ggsave(here("figures","p5.png"), p5)
 ggsave(here("figures","p6.png"), p6)
+ggsave(here("figures","p7.png"), p7)
+ggsave(here("figures","p8.png"), p8)
+
+
+(deltas <- runs_long %>% mutate(deltaTM = (time - filter(runs_long, solver == 'gurobi')$time)/filter(runs_long, solver == 'gurobi')$time * 100,
+                             deltaTT = time - filter(runs_long, solver == 'gurobi')$time,
+                             deltaC = (cost - filter(runs_long, solver == 'gurobi')$cost)/filter(runs_long, solver == 'gurobi')$cost * 100
+                             ) %>% group_by(solver) %>% summarise(avg_time = mean(deltaTM)/100,
+                                                                  max_time = max(deltaTM)/100,
+                                                                  min_time = min(deltaTM)/100,
+                                                                  avg_cost = mean(deltaC, na.rm = T),
+                                                                  max_cost = max(deltaC, na.rm = T),
+                                                                  min_cost = min(deltaC, na.rm = T))
+) 
 
