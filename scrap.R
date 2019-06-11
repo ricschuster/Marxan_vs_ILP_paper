842a95e3aafdd8640cb3c438a638e6c0d37394fc
ricschuster <mail@richard-schuster.com>
2019-06-11 13:37:16 -0700

    tata

--- scrap.R
+++ scrap.R
@@ -12,7 +12,7 @@ select <- dplyr::select
 walk(list.files("R", full.names = TRUE), source)
 prioritizr_timed <- add_timer(prioritizr::solve)
 # parallelization
-n_cores <- 5
+n_cores <- 8
 cl <- makeCluster(n_cores)
 registerDoParallel(cl)
 
@@ -167,7 +167,8 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
                         # data
                         m_data@species$spf <- r_marxan$spf
                         # options
-                        m_opts <- MarxanOpts(BLM = r$blm, NCORES = 1L, VERBOSITY = 3L)
+                        m_opts <- MarxanOpts(NCORES = 1L, VERBOSITY = 3L)
+                        m_opts@BLM <- as.integer(r$blm)
                         m_opts@NUMREPS <- as.integer(marxan_reps)
                         m_opts@NUMITNS <- as.integer(r_marxan$marxan_iterations)
                         m_opts@NUMTEMP <- as.integer(ceiling(m_opts@NUMITNS * 0.2))
