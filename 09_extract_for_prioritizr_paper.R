c87debff19238432119038a3c07dfbadab72a711
ricschuster <mail@richard-schuster.com>
2020-11-05 13:09:18 -0800

    tata

--- 09_extract_for_prioritizr_paper.R
+++ 09_extract_for_prioritizr_paper.R
@@ -1,41 +1,96 @@
 library(raster)
 library(prioritizr)
-library(marxan)
-library(foreach)
-library(doParallel)
 library(uuid)
 library(here)
 library(tidyverse)
 
-pkg_list <- c("raster", "prioritizr", "marxan", "uuid",  "here", "tidyverse")
-select <- dplyr::select
-walk(list.files("R", full.names = TRUE), source)
-prioritizr_timed <- add_timer(prioritizr::solve)
-# parallelization
-n_cores <- 8
-cl <- makeCluster(n_cores)
-registerDoParallel(cl)
+# pkg_list <- c("raster", "prioritizr", "marxan", "uuid",  "here", "tidyverse")
+# select <- dplyr::select
+# walk(list.files("R", full.names = TRUE), source)
+# prioritizr_timed <- add_timer(prioritizr::solve)
+# # parallelization
+# # n_cores <- 12
+# # cl <- makeCluster(n_cores)
+# # registerDoParallel(cl)
+# 
+# # load nplcc data ----
+# 
+# # species list
+# species <- here("data", "nplcc_species.csv") %>% 
+#   read_csv() %>% 
+#   mutate(id = as.integer(id))
+# 
+# # cost and occupancy
+# nplcc_file <- here("data", "nplcc_cost_occupancy.zip")
+# if (!file.exists(nplcc_file)) {
+#   "https://s3.amazonaws.com/marxan-vs-ilp/nplcc_cost_occupancy.zip" %>% 
+#     download.file(destfile = nplcc_file)
+# }
+# cost_occ <- read_csv(nplcc_file, 
+#                      col_types = cols(.default = col_double(),
+#                                       pu = col_integer()))
+# 
+# # split out cost and occupancy
+# cost <- select(cost_occ, id = pu, cost) %>% 
+#   arrange(id)
+# 
+# 
+# tt <- here("data", "nplcc_planning-units.tif") %>% 
+#   raster()
+# 
+# tt[] <- 1:ncell(tt)
+# 
+# e <- extent(560000, 560000 + 22500, 5300000 - 22500, 5300000)
+# tmp.r <- crop(tt, e)
+# 
+# df_red <- tibble(pu = tmp.r[]) %>%
+#   left_join(cost_occ, by = "pu")
+# 
+# ll <- list()
+# for(ii in 1:ncol(df_red)){
+#   ll[[ii]] <- tmp.r
+#   values(ll[[ii]]) <- df_red %>% pull(ii)
+#   names(ll[[ii]]) <- names(df_red)[ii]
+# }
+# ll2 <- stack(ll)
+# 
+# # writeRaster(ll2, )
+# writeRaster(ll2, filename = here("/tmp", names(ll2)) , bylayer=TRUE,format="GTiff")
+# 
 
-# load nplcc data ----
+#END PREP
+ll2 <- list.files(here("tmp"), pattern = ".tif$", full.names = TRUE) %>%
+  stack() %>%
+  readAll()
 
-# species list
-species <- here("data", "nplcc_species.csv") %>% 
-  read_csv() %>% 
-  mutate(id = as.integer(id))
 
-# cost and occupancy
-nplcc_file <- here("data", "nplcc_cost_occupancy.zip")
-if (!file.exists(nplcc_file)) {
-  "https://s3.amazonaws.com/marxan-vs-ilp/nplcc_cost_occupancy.zip" %>% 
-    download.file(destfile = nplcc_file)
-}
-cost_occ <- read_csv(nplcc_file, 
-                     col_types = cols(.default = col_double(),
-                                      pu = col_integer()))
+con_problem <-
+  prioritizr::problem(ll2$cost, round(ll2 %>% dropLayer(c(1,2)), 4)) %>%
+  prioritizr::add_min_set_objective() %>%
+  prioritizr::add_relative_targets(0.2) %>%
+  # prioritizr::add_locked_in_constraints(con_pa_raster_data) %>%
+  prioritizr::add_binary_decisions() %>%
+  prioritizr::add_gurobi_solver(gap = 0.01,
+                                threads = 1,
+                                first_feasible = FALSE)
 
-# split out cost and occupancy
-cost <- select(cost_occ, id = pu, cost) %>% 
-  arrange(id)
+
+boundary_penalties <- c(0, 100, 1000)
+
+con_boundary_raster_solutions <-
+  boundary_penalties %>%
+  lapply(function(x) {
+    con_problem %>%
+      prioritizr::add_boundary_penalties(x, edge_factor = 0.5) %>%
+      prioritizr::solve(force = TRUE)
+  }) %>% raster::stack()
+raster::plot(con_boundary_raster_solutions)
+
+#END TESTING
+
+
+
+
 occ <- select(cost_occ, -cost) %>% 
   gather("species_code", "amount", -pu) %>% 
   inner_join(species %>% select(species_code, species = id), 
@@ -81,16 +136,16 @@ stopifnot(file.exists(marxan_path))
 # iterate over runs ----
 
 # clean up old files
-gurobi_dir <- here("output_blm2", "gurobi")
+gurobi_dir <- here("output_blm", "gurobi")
 #unlink(gurobi_dir, recursive = TRUE)
 dir.create(gurobi_dir)
-rsymphony_dir <- here("output_blm2", "rsymphony")
+rsymphony_dir <- here("output_blm", "rsymphony")
 #unlink(rsymphony_dir, recursive = TRUE)
 dir.create(rsymphony_dir)
-marxan_dir <- here("output_blm2", "marxan")
+marxan_dir <- here("output_blm", "marxan")
 #unlink(marxan_dir, recursive = TRUE)
 dir.create(marxan_dir)
-runs_dir <- here("output_blm2", "runs")
+runs_dir <- here("output_blm", "runs")
 #unlink(runs_dir, recursive = TRUE)
 dir.create(runs_dir)
 
@@ -124,11 +179,11 @@ bnd_df <- data.frame(id1 = tmp.r[][smm_mat$i],
                      id2 = tmp.r[][smm_mat$j],
                      amount = round(smm_mat$x,0))
 
-runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg_list) %dopar% {
+runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows) %do% {
   r <- runs[run, ]
-  # str_glue_data(r, "Run ", run, 
-  #               ": Target {target}; Features {n_features}; PUs {n_pu}; BLM {blm}") %>% 
-  #   message()
+  str_glue_data(r, "Run ", run, 
+                ": Target {target}; Features {n_features}; PUs {n_pu}; BLM {blm}") %>% 
+    message()
   
   
   
@@ -148,18 +203,56 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
     transmute(id = species, amount = r$target * tot_amount)
   features <- inner_join(features, targets, by = "id")
   
-
-  # marxan
+  # ilp 
+  p <- problem(cost_ss, 
+               features = features %>% select(id, name), 
+               rij = rij, 
+               cost_column = "cost") %>% 
+    add_min_set_objective() %>%
+    add_relative_targets(r$target) %>%
+    add_binary_decisions() %>%
+    add_boundary_penalties(penalty = r$blm, edge_factor = 0.5, data = bnd_mat)
+  # gurobi
+  s_gur <- p %>% 
+    add_gurobi_solver(gap = ilp_gap) %>%
+    prioritizr_timed(force = TRUE)
+  # solution summary
+  cost_gurobi <- attr(s_gur$result, "objective")
+  r$gurobi <- list(tibble(n_solutions = 1,
+                          cost = cost_gurobi, 
+                          time = s_gur$time[["elapsed"]]))
+  # save solution
+  s_gur <- "gurobi_target-{target}_features-{n_features}_pu-{n_pu}_blm-{blm}.tif" %>% 
+    str_glue_data(r, .) %>% 
+    file.path(gurobi_dir, .) %>% 
+    writeRaster(solution_to_raster(s_gur$result, pus), overwrite = TRUE, .)
+  rm(s_gur)
+  
+  # symphony
+  s_sym <- p %>% 
+    add_rsymphony_solver(gap = ilp_gap * cost_gurobi) %>% 
+    prioritizr_timed(force = TRUE)
+  # solution summary
+  r$rsymphony <- list(tibble(n_solutions = 1,
+                             cost = attr(s_sym$result, "objective"), 
+                             time = s_sym$time[["elapsed"]]))
+  # save solution
+  s_sym <- "rsymphony_target-{target}_features-{n_features}_pu-{n_pu}_blm-{blm}.tif" %>% 
+    str_glue_data(r, .) %>% 
+    file.path(rsymphony_dir, .) %>% 
+    writeRaster(solution_to_raster(s_sym$result, pus), overwrite = TRUE, .)
+  rm(s_sym)
+  
+  #marxan
   m_data <- MarxanData(pu = mutate(cost_ss, status = 0L),
                        species = features %>%
                          mutate(spf = 1) %>%
                          select(id, target = amount, spf, name),
                        puvspecies = select(rij, species, pu, amount),
                        boundary = bnd_df, skipchecks = TRUE)
-  # loop over marxan iterations
-  i_marxan <- 1
-  # r_marxan <- foreach(i_marxan = seq_len(nrow(r$marxan[[1]])),
-  #                     .combine = bind_rows, .packages = pkg_list) %dopar% {
+  #loop over marxan iterations
+  r_marxan <- foreach(i_marxan = seq_len(nrow(r$marxan[[1]])),
+                      .combine = bind_rows, .packages = pkg_list) %dopar% {
                         r_marxan <- r$marxan[[1]][i_marxan, , drop = FALSE]
                         message(paste0("  Marxan: ",
                                        "SPF = ", r_marxan$spf, "; ",
@@ -167,8 +260,7 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
                         # data
                         m_data@species$spf <- r_marxan$spf
                         # options
-                        m_opts <- MarxanOpts(NCORES = 1L, VERBOSITY = 3L)
-                        m_opts@BLM <- as.integer(r$blm)
+                        m_opts <- MarxanOpts(BLM = r$blm, NCORES = 1L, VERBOSITY = 3L)
                         m_opts@NUMREPS <- as.integer(marxan_reps)
                         m_opts@NUMITNS <- as.integer(r_marxan$marxan_iterations)
                         m_opts@NUMTEMP <- as.integer(ceiling(m_opts@NUMITNS * 0.2))
@@ -194,9 +286,9 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
                           r_marxan$n_solutions <- sum(m_results@summary$Shortfall == 0)
                           if (r_marxan$n_solutions > 0) {
                             best <- filter(m_results@summary, Shortfall == 0) %>%
-                              arrange(Cost) %>%
+                              arrange(Score) %>%
                               slice(1)
-                            r_marxan$cost <- best$Cost
+                            r_marxan$cost <- best$Score
                             # raster solution
                             s_mar <- m_results@selections[best$Run_Number,] %>%
                               as.data.frame() %>%
@@ -208,7 +300,7 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
                                           "marxan_target-{target}_features-{n_features}_pu-{n_pu}_blm-{blm}_",
                                           "spf-{spf}_iters-{marxan_iterations}.tif") %>%
                               file.path(marxan_dir, .) %>%
-                              writeRaster(s_mar, .)
+                              writeRaster(s_mar, overwrite = TRUE, .)
                             rm(s_mar)
                           } else {
                             r_marxan$cost <- NA_real_
@@ -219,26 +311,15 @@ runs <- foreach(run = seq_len(nrow(runs)), .combine = bind_rows, .packages = pkg
                           r_marxan$cost <- NA_real_
                           r_marxan$time <- NA_real_
                         }
-                        r_marxan <- select(r_marxan, marxan_iterations, spf, n_solutions, cost, time)
-                      # }
+                        select(r_marxan, marxan_iterations, spf, n_solutions, cost, time)
+                      }
   r$marxan <- list(r_marxan)
   # save this iteration in case of crashing
-  str_glue_data(r, "Marxan_run-", run,
+  str_glue_data(r, "run-", run,
                 "_target-{target}_features-{n_features}_pu-{n_pu}_blm-{blm}.rds") %>%
     file.path(runs_dir, .) %>%
     saveRDS(r, .)
-   r
-}
-
-setwd(here("output_blm2/runs/"))
-fls <- list.files()
-runs_2 <- readRDS(fls[1])
-
-for(ii in 2:length(fls)){
-  tmp <- readRDS(fls[ii])
-  runs_2 <- rbind(runs_2, tmp)
-  
-  rm(tmp)
+  r
 }
 
 # unnest
@@ -250,14 +331,16 @@ runs_s <- runs %>%
   mutate(solver = "rsymphony") %>% 
   select(run_id, solver, target, n_features, n_pu, species, rsymphony) %>% 
   unnest()
-runs_m <- runs_2 %>% 
-  mutate(solver = "marxan") %>% 
-  select(run_id, solver, target, n_features, n_pu, species, blm, marxan) %>% 
-  unnest()
-runs_long <- bind_rows(runs_g, runs_s, runs_m)
-write_csv(runs_m, "ilp-comparison-runs_marx.csv")
+# runs_m <- runs %>% 
+#   mutate(solver = "marxan") %>% 
+#   select(run_id, solver, target, n_features, n_pu, species, marxan) %>% 
+#   unnest()
+runs_long <- bind_rows(runs_g, runs_s)
+# runs_long <- bind_rows(runs_g, runs_s, runs_m)
+write_csv(runs_long, here("output_blm", "ilp-comparison-runs_no_marx.csv"))
 
 # clean up
 stopCluster(cl)
+
 
 
